eef51a37f2e1f53de1eb401f9ff73d1e1784ad2c
aa9029 <103806179+aa9029@users.noreply.github.com>
2022-11-23 16:09:04 +0530

    Add files via upload

---
+++ W10Q1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int a,b;
+
+   cout <<" Enter two numbers:";
+   cin>>a>>b;
+   try {
+      
+      if (b== 0) {          
+      cout << "Division="<<a,b;
+      else
+         throw var;
+      
+   } catch (int) {
+      
+      cout << "Exception caught division by zero";
+   }
+     return 0;
+}
---
+++ W10Q2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main() {
+   int var = 0;
+
+   cout << "Simple C++ Program for Catch All or Default Exception Handling\n";
+  
+   try {
+      
+      if (var == 0) {
+         throw var;
+         
+      }
+   }   
+   catch (float ex) {
+     
+      cout << "Float Exception catch : Value :" << ex;
+   } catch (...) {
+      
+      cout << "Default Exception Catch";
+   }
+   getch();
+   return 0;
+}
